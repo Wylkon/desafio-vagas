a98c3b012f6673c44b4fd73203fdbfb9c16d9bf7
Wylkon Cardoso <wylkon@wylkon.com.br>
2016-03-25 23:18:03 -0300

    Implementing home component

--- src/javascript/components/Home.js
+++ src/javascript/components/Home.js
@@ -1,13 +1,30 @@
-import React from 'react';
+var React = require('react');
+var VideoDetail = require('./VideoDetail');
 
 var Home = React.createClass({
   render: function() {
     return (
-      <h2 className="text-center">
-        Vídeo em destaque
-      </h2>
+      <div>
+          <section className="fi-medium-wrap">
+              <h2 className="fi-headline">Vídeo em destaque</h2>
+              <VideoDetail />
+              {/*<VideoDetail video={this.state.selectedVideo} />*/}
+          </section>
+          <section className="fi-small-wrap">
+              <h2 className="fi-headline">+Vídeos</h2>
+              <div className="fi-video-list">
+                  {/*<VideoList
+                    handleVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                  />
+                  <div className="fi-wrap-load">
+                      <button className="fi-load-more" onClick={() => this.loadMore(this.state.term, this.state.nextPageToken)}>carregar mais vídeos...</button>
+                  </div>*/}
+              </div>
+          </section>
+      </div>
     )
   }
-})
+});
 
 module.exports = Home;
